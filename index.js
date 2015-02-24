b72bd0a241e46c9d956371d3ff3ae36b946f1a26
Cecil Worsley <cworsley@redventures.com>
2015-02-24 12:09:25 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+
+var supercache = require('./lib');
+module.exports = supercache;
---
+++ tests/index.js
@@ -0,0 +1,10 @@
+
+var supercache = require('../lib');
+
+describe('testing supercache', function () {
+
+  it('should return all supertest functions', function (done) {
+    done();
+  });
+
+});
