cec1f53fad56780a7f48512b8100ce031048522b
Cecil Worsley <cworsley@redventures.com>
2015-02-25 00:24:26 -0500

    first working version

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,17 @@
 
 var supercache = require('../lib');
+var redis = require('articulate-server-sms-resources').conn.redis.getFresh();
+var debug = require('debug')('supercache-test');
 
 describe('testing supercache', function () {
 
   it('should return all supertest functions', function (done) {
-    done();
+    supercache(redis, 'test-cache')
+      .get('http://testsms01.articulate.io')
+      .end(function (response) {
+        debug(arguments, response);
+        done();
+      });
   });
 
 });
