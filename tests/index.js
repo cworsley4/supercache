77c76128310fcd86f65edc7fbaa1ad093f64125c
Cecil Worsley <cworsley@redventures.com>
2015-02-25 00:52:35 -0500

    adding a couple tests

--- tests/index.js
+++ tests/index.js
@@ -1,17 +1,37 @@
 
+var redis = require('redis');
 var supercache = require('../lib');
-var redis = require('articulate-server-sms-resources').conn.redis.getFresh();
+var redisClient = redis.createClient();
+var should = require('should');
 var debug = require('debug')('supercache-test');
+
+var standard = function () {
+  return supercache(redisClient, 'test-cache', 500)
+    .get('http://www.google.com');
+};
 
 describe('testing supercache', function () {
 
-  it('should return all supertest functions', function (done) {
-    supercache(redis, 'test-cache')
-      .get('http://testsms01.articulate.io')
-      .end(function (response) {
-        debug(arguments, response);
+  before(function (done) {
+    redisClient.flushdb(done);
+  });
+
+  it('should get and set a response', function (done) {
+    standard()
+      .end(function (err, response, fromCache) {
+        should.not.exist(err);
+        (fromCache === undefined).should.be.true;
         done();
-      });
+    });
+  });
+
+  it('should get reponse from cache', function (done) {
+    standard()
+      .end(function (err, response, fromCache) {
+        should.not.exist(err);
+        fromCache.should.be.true;
+        done();
+    });
   });
 
 });
